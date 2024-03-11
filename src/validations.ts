282aebb7d5923fb5f24d0e80c1b83aa3d69edd06
Vladimir <v.borovik@1inch.io>
2024-03-11 21:13:24 +0300

    chore: export `Address` and `getLimitOrderContract` (#9)

--- src/validations.ts
+++ src/validations.ts
@@ -1,13 +1,3 @@
-export function isValidAmount(value: string | bigint): boolean {
-    try {
-        const amount = BigInt(value)
-
-        return amount >= 0n
-    } catch (e) {
-        return false
-    }
-}
-
 export function isInt(val: number): boolean {
     return Math.floor(val) === val
 }
