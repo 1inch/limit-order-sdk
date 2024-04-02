231f7fe14b0154887c3a0b83274011aebc7a4495
Vladimir <v.borovik@1inch.io>
2024-04-02 19:49:08 +0300

    fix(limit-order): limit max salt and amounts (#20)

---
+++ src/utils/rand-bigint.spec.ts
@@ -0,0 +1,14 @@
+import {randBigInt} from './rand-bigint'
+
+describe('randBigint', () => {
+    it('should generate rand bigint in correct interval', () => {
+        expect(randBigInt(1)).toBeGreaterThanOrEqual(0n)
+        expect(randBigInt(1)).toBeLessThanOrEqual(1n)
+
+        expect(randBigInt(10n)).toBeGreaterThanOrEqual(0)
+        expect(randBigInt(10n)).toBeLessThanOrEqual(10n)
+
+        expect(randBigInt(2n << 96n)).toBeGreaterThanOrEqual(0)
+        expect(randBigInt(2n << 96n)).toBeLessThanOrEqual(2n << 96n)
+    })
+})
