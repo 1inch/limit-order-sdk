faba43483692e555a96377d0ceb7efad739f59cb
Vladimir <v.borovik@1inch.io>
2024-04-12 17:11:00 +0300

    feat(interaction): add `.decode` method (#24)

--- src/limit-order/interaction.ts
+++ src/limit-order/interaction.ts
@@ -1,10 +1,21 @@
-import {isHexBytes, trim0x} from '@1inch/byte-utils'
+import {BytesIter, isHexBytes, trim0x} from '@1inch/byte-utils'
 import assert from 'assert'
 import {Address} from '../address'
 
 export class Interaction {
     constructor(public readonly target: Address, public readonly data: string) {
         assert(isHexBytes(data), 'Interaction data must be valid hex bytes')
+    }
+
+    /**
+     * Create `Interaction` from bytes
+     *
+     * @param bytes Hex string with 0x. First 20 bytes are target, then data
+     */
+    public static decode(bytes: string): Interaction {
+        const iter = BytesIter.String(bytes)
+
+        return new Interaction(new Address(iter.nextUint160()), iter.rest())
     }
 
     /**
