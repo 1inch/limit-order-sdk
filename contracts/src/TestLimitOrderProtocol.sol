f24c4ce51e3e93589f3563d0ab7cbc0dbf300377
Vladimir Borovik <v.borovik@1inch.io>
2025-01-16 14:40:06 +0300

    wipˆ

---
+++ contracts/src/TestLimitOrderProtocol.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.23;
+
+import "../lib/limit-order-protocol/contracts/LimitOrderProtocol.sol";
+
+contract TestLimitOrderProtocol is LimitOrderProtocol {
+    constructor(IWETH weth) CrossChainResolver(weth) {}
+}
